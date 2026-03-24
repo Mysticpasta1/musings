5f33630bf304e9f7e73bf4c5167288128d95753b
Mysticpasta1 <ethana5tc@outlook.com>
2026-03-24 13:26:34 -0500

    26.1 start

--- src/main/java/com/mystic/musings/MusingsClient.java
+++ src/main/java/com/mystic/musings/MusingsClient.java
@@ -4,11 +4,17 @@ import com.mystic.musings.init.ParticleInit;
 import com.mystic.musings.particles.BaseInkTear;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 
-@EventBusSubscriber(value = Dist.CLIENT)
-public class ClientSetup {
+@Mod(value = Musings.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = Musings.MODID, value = Dist.CLIENT)
+public class MusingsClient {
+    public MusingsClient(ModContainer container) {
+    }
+
     @SubscribeEvent
     public static void onRegisterParticles(RegisterParticleProvidersEvent e) {
         e.registerSpriteSet(ParticleInit.INK_TEAR_HANG.get(), sprites ->
