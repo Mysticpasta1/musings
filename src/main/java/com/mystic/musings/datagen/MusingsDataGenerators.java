f8a54bd7c935b4a509bfd60988331e6de459f98b
Mysticpasta1 <ethana5tc@outlook.com>
2026-03-24 23:29:05 -0500

    fix data again

--- src/main/java/com/mystic/musings/datagen/MusingsDataGenerators.java
+++ src/main/java/com/mystic/musings/datagen/MusingsDataGenerators.java
@@ -15,21 +15,13 @@ import java.util.concurrent.CompletableFuture;
 @EventBusSubscriber(modid = Musings.MODID)
 public class MusingsDataGenerators {
     @SubscribeEvent
-    public static void gatherData(final GatherDataEvent.Server event) {
+    public static void gatherData(final GatherDataEvent.Client event) {
         DataGenerator gen = event.getGenerator();
         PackOutput packOutput = gen.getPackOutput();
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
         gen.addProvider(true, new MusingsRecipes(packOutput, lookupProvider));
         gen.addProvider(true, new MusingLootTables(packOutput, Set.of(), List.of(), lookupProvider));
-    }
-
-    @SubscribeEvent
-    public static void gatherData(final GatherDataEvent.Client event) {
-        DataGenerator gen = event.getGenerator();
-        PackOutput packOutput = gen.getPackOutput();
-        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-        MusingsBlockTagsProvider blockTags = new MusingsBlockTagsProvider(packOutput, lookupProvider);
-        gen.addProvider(true, blockTags);
+        gen.addProvider(true, new MusingsBlockTagsProvider(packOutput, lookupProvider));
         gen.addProvider(true, new MusingsItemTagsProvider(packOutput, lookupProvider));
         gen.addProvider(true, new MusingsLanguageProvider(packOutput));
         gen.addProvider(true, new MusingsModelProvider(packOutput, Musings.MODID));
